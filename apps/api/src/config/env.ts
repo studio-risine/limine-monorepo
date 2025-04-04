1bd8e38da43bce95ee80b0b11d8e9094c8f96f59
Gabriel Melo <gabriel.smelo@proton.me>
2025-04-04 18:29:52 -0300

    feat(api): setup swagger

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,17 +1,16 @@
-
 import { z } from 'zod'
 
 const envSchema = z.object({
-  HOST: z.string().default('0.0.0.0'),
-  PORT: z.coerce.number().default(3333),
+	HOST: z.string().default('0.0.0.0'),
+	PORT: z.coerce.number().default(3333),
 })
 
 const { data, success, error } = envSchema.safeParse(process.env)
 
 if (success === false) {
-  console.error('❌ Invalid environment variables', error.format())
+	console.error('❌ Invalid environment variables', error.format())
 
-  throw new Error('Invalid environment variables.')
+	throw new Error('Invalid environment variables.')
 }
 
 export const env = data
