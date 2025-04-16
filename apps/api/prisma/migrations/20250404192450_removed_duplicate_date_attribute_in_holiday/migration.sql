479d6cef09291139d4675fea2f1c293a7edc2884
Gabriel Melo <gabriel.smelo@proton.me>
2025-04-16 01:50:30 -0300

    chore: setup config vitest and other tools

--- apps/api/prisma/migrations/20250404192450_removed_duplicate_date_attribute_in_holiday/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `data` on the `holidays` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "holidays" DROP COLUMN "data";
--- apps/api/prisma/migrations/20250404204459_add_name_in_deadline_schema/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "deadlines" ADD COLUMN     "name" TEXT;
